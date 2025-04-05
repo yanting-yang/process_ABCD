3a61573bfa2fd12817f4eef4b3041209b4999487
Yanting Yang <yantingyang2024@gmail.com>
2025-04-05 01:39:13 -0700

    sync

--- scripts/run_slurm.sh
+++ scripts/run_slurm.sh
@@ -3,25 +3,16 @@
 INPUT_DIR="$HOME/datasets/ABCD"
 OUTPUT_DIR="$HOME/datasets/ABCD_DiFuMo1024"
 
-# Initialize an index counter
 index=0
 array=""
 
-# Find all .tgz files in the input directory and its subdirectories
 files=$(find "$INPUT_DIR" -type f -name "*.tgz")
 for file in $files; do
-    # Extract the base filename without extension and path
     base_name=$(basename "$file" .tgz)
-
-    # Define the expected .npy filename in the output directory
     npy_file="$OUTPUT_DIR/${base_name}.npy"
-    # Check if the .npy file exists
-    if [ -f "$npy_file" ]; then
-        # .npy file exists, skip this .tgz file
-        continue
-    else
-        # .npy file does not exist, output the index and the .tgz file path separated by a comma
+    if [ ! -f "$npy_file" ]; then
         array+="$index,"
+        echo "File $file does not exist, adding to array index $index"
     fi
     index=$((index + 1))
 done
