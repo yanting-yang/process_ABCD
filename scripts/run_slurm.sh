8cbca5985d4b2ed70f3a070838469ecc00bce13f
Yanting Yang <yantingyang2024@gmail.com>
2025-04-05 01:46:05 -0700

    sync

--- scripts/run_slurm.sh
+++ scripts/run_slurm.sh
@@ -4,20 +4,21 @@ INPUT_DIR="$HOME/datasets/ABCD"
 OUTPUT_DIR="$HOME/datasets/ABCD_DiFuMo1024"
 
 index=0
-array=""
+array=()
 
 files=$(find "$INPUT_DIR" -type f -name "*.tgz")
 for file in $files; do
-    base_name=$(basename "$file" .tgz)
+    base_name="${file##*/}"
+    base_name="${base_name%.tgz}"
     npy_file="$OUTPUT_DIR/${base_name}.npy"
     if [ ! -f "$npy_file" ]; then
-        array+="$index,"
+        array+=("$index")
         echo "File $file does not exist, adding to array index $index"
     fi
-    index=$((index + 1))
+    ((index++))
 done
 
-array=${array%,}
+array_str=$(IFS=, ; echo "${array[*]}")
 
 sbatch \
     --array=$array \
